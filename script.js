049c3827709b2c613eec532bcfb4fec851640920
ebonini <44753902+ebonini@users.noreply.github.com>
2024-11-25 17:14:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 const urlParams = new URLSearchParams(window.location.search);
 const videoId = urlParams.get('id');
 
-// URL base do Mega (substitua isso pela URL base correta do Mega)
-const megaBaseUrl = "https://mega.nz/file/";
+// URL base do Google Drive
+const googleDriveBaseUrl = "https://drive.google.com/uc?export=download&id=";
 
-// Gera a URL completa do vídeo no Mega
-const megaVideoUrl = `${megaBaseUrl}${videoId}`;
+// Gera a URL completa do vídeo no Google Drive
+const googleDriveVideoUrl = `${googleDriveBaseUrl}${videoId}`;
 
-// Redireciona para o vídeo no Mega
+// Redireciona para o vídeo no Google Drive
 if (videoId) {
-    window.location.href = megaVideoUrl;
+    window.location.href = googleDriveVideoUrl;
 } else {
     document.body.innerHTML = "<h1>Erro: ID do vídeo não fornecido.</h1>";
 }
